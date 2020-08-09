f8b6aff3c6a029a3910e587acab3df525565a8d2
wangye <git@github.com:wangyGit/sad_Sad.git>
2020-08-09 20:26:50 +0800

    Initial commit

---
+++ src/main/java/com/ibss/ApplicationStrartup.java
@@ -0,0 +1,17 @@
+package com.ibss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangy
+ * @Data 2020/7/28 13:58
+ * @Description
+ */
+@SpringBootApplication
+public class ApplicationStrartup
+{
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStrartup.class, args);
+    }
+}
